9ef777de910affe09bdbc0c49c91de8e660c8df2
acesmndr <acesmndr@gmail.com>
2014-02-27 18:22:21 +0545

    Highcharts compatibility

--- js/Teach.js
+++ js/Teach.js
@@ -1,98 +1,74 @@
-var chart;
-		var graph;
-		var chartCursor;
-            var chartData=[];
-		var chartData2=[];
-		         
-		$.getJSON("js/teach.json",function(data){
-			chartData2=data;
-			$.each(data,function(key,val){
-		if(val.year==2011){
-		chartData.push(val);
-		}		
-		});
-				
-		});
-		
-		/*for(var i=0;i<chartData.length;i++)
-		if(chartData[i].year==2008){
-			chartData1.push(chartData[i]);
-		}*/
-		
-		
-		
-            AmCharts.ready(function () {
-                // SERIAL CHART
-                chart = new AmCharts.AmSerialChart();
-                chart.dataProvider = chartData;
-                chart.categoryField = "district";
-                chart.startDuration = 1;
+	var stotal=[];
+	var dis=[];
+	var addd;
+	var ptotal=[];	
+	var options={
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Teach Nepal'
+            },
+            subtitle: {
+                text: 'Source: data.opennepal.net'
+            },
+            xAxis: {
+                categories: [],
+		labels:{
+                            enabled: false
+                        }
 
-                // AXES
-                // category
-               /*var categoryAxis = chart.categoryAxis;
-                categoryAxis.labelRotation = 90;
-                categoryAxis.gridPosition = "start";
-*/
-                // value
-                // in case you don't want to change default settings of value axis,
-                // you don't need to create it, as one value axis is created automatically.
-
-                // GRAPH
-                graph = new AmCharts.AmGraph();
-                graph.valueField = "total";
-                graph.balloonText = "[[category]]: <b>[[value]]</b>";
-                graph.type = "column";
-                graph.lineAlpha = 0;
-                graph.fillAlphas = 0.9;
-		
-                chart.addGraph(graph);
-		
-                // CURSOR
-                chartCursor = new AmCharts.ChartCursor();
-                chartCursor.cursorAlpha = 0;
-                chartCursor.zoomable = false;
-                chartCursor.categoryBalloonEnabled = false;
-                chart.addChartCursor(chartCursor);
-
-                chart.creditsPosition = "top-right";
-
-                chart.write("chartdiv");
-            });
-$("#div1").click(function(){
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (mm)'
+                },
+		            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.0f}<! mm--></b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+			dataLabels: {
+                                enabled: false
+                            },		
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series:[{data:[]
+	}]        
+	};
 	$.getJSON("js/teach.json",function(data){
-		chartData.length=0;
+			addd=data;
 		$.each(data,function(key,val){
-		if(val.year==2008){
-		chartData.push(val);
-		}
-		});
-		});
-	/*chart = new AmCharts.AmSerialChart();
-                chart.dataProvider = chartData;
-                chart.categoryField = "district";
-                chart.startDuration = 1;	
-	graph = new AmCharts.AmGraph();
-                graph.valueField = "total";
-                graph.balloonText = "[[category]]: <b>[[value]]</b>";
-                graph.type = "column";
-                graph.lineAlpha = 0;
-                graph.fillAlphas = 0.9;
-		chart.addGraph(graph);
-		
-                // CURSOR
-                chartCursor = new AmCharts.ChartCursor();
-                chartCursor.cursorAlpha = 0;
-                chartCursor.zoomable = false;
-                chartCursor.categoryBalloonEnabled = false;
-                chart.addChartCursor(chartCursor);
+			
+			console.log(val.total,val.district);
+			if(val.year==2011){
+			stotal.push(parseInt(val.total));
+			dis.push(val.district);
+			}else{
+			if(val.year==2010){
+				ptotal.push(parseInt(val.total));
+				}}
+				});	
+	}); 
+	console.log(options.series[0]);
+	options.series.push({name:2010,data:ptotal});	
+	options.xAxis.categories=dis;
+	console.log("cat ",options.xAxis.categories);
+	options.series[0].name="2011";
+	options.series[0].data=stotal;
+	console.log("data ",options.series[0].data);
+	$(function () {        
+	$('#container').highcharts(options);
+	console.log("ptotal",ptotal);
+    });
+    
 
-                chart.creditsPosition = "top-right";
-		
-chart.write("chartdiv");*/
-		chart.dataProvider = chartData;
-		chart.validateData();
-		chart.validateNow();
-                
-	});
-$("document").ready(function(){});
